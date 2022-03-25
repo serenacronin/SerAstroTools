b3bb13770601f7c0070774be2b8a2160ba10a7a0
Serena A. Cronin <cronin.104@osu.edu>
2022-03-25 18:29:55 -0400

    gauss_tools for julia

---
+++ gauss_tools.jl
@@ -0,0 +1,23 @@
+"""
+Function that will create a two-Gaussian model from input parameters
+Input: x-axis array and input parameters (i.e., the initial guesses of data you may be trying to fit) 
+for both Gaussians (amplitudes, centroids, and sigmas)
+Output: two-Gaussian model
+"""
+function two_gaussian(x_array, amp1,cen1,sigma1, amp2,cen2,sigma2)
+    gaussian1 = amp1 .* (exp.((-1.0/2.0) .* (((x_array .- cen1) ./ sigma1).^2.0)));
+    gaussian2 = amp2 .* (exp.((-1.0/2.0) .* (((x_array .- cen2) ./ sigma2).^2.0)));
+
+    gaussian1 .+ gaussian2;
+end
+
+"""
+Function that will create a Gaussian model from input parameters
+Input: x-axis array and input parameters (i.e., the initial guesses of data you may be trying to fit) 
+for both Gaussians (amplitude, centroid, and sigma)
+Output: Gaussian model
+"""
+function one_gaussian(x_array, amp,cen,sigma)
+    amp .* (exp.((-1.0/2.0) .* (((x_array .- cen) ./ sigma).^2.0)));
+end
+
